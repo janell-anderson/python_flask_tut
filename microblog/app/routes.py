f4c66b54eec365b28022b89e5290c28b844f1436
Janell Anderson <andersonjanell@gmail.com>
2018-11-27 22:54:52 -0500

    Added forms using a flask-wtforms extension for a login page consisting of username, password, remember me and submit field.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,8 +1,13 @@
-from app import app
 from flask import render_template
+from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 def index():
     user = {'username': 'Renee'}
     posts= [
